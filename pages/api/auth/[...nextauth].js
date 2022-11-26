f81e1929e88b916bd9ed5e87b01da8670fa97e7b
Fábio <fabio.oliveira@poscontrole.com.br>
2022-11-26 18:49:10 -0300

    my email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,7 +19,10 @@ export default NextAuth({
   callbacks: {
     async signIn({ account, profile }) {
       if (account.provider === "google") {
-        return profile.email_verified; // && profile.email.endsWith("@example.com")
+        // return profile.email_verified && profile.email.endsWith("@example.com")
+        return (
+          profile.email_verified && profile.email === "fabiojiqui@gmail.com"
+        );
       }
       return true; // Do different verification for other providers that don't have `email_verified`
     },
