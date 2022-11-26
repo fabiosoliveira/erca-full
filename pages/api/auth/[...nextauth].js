85472cb219a0d90df30f786e8bfbe25098b26422
Fábio <fabio.oliveira@poscontrole.com.br>
2022-11-26 18:40:56 -0300

    callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,4 +16,12 @@ export default NextAuth({
     }),
   ],
   secret: process.env.JWT_SECRET,
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account.provider === "google") {
+        return profile.email_verified; // && profile.email.endsWith("@example.com")
+      }
+      return true; // Do different verification for other providers that don't have `email_verified`
+    },
+  },
 });
