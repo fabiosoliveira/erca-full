6a03684567e3f8f11401859af321117df54b3bd6
Fábio <fabio.oliveira@poscontrole.com.br>
2022-11-28 09:07:41 -0300

    update

--- pages/logout.jsx
+++ pages/logout.jsx
@@ -2,7 +2,6 @@ import { signOut, useSession } from "next-auth/react";
 
 export default function Logout() {
   const { data: session } = useSession({ required: true });
-  //j
 
   return (
     <div
